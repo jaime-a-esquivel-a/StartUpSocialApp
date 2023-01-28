2907a6f6d01050705c6d1f08e7c4bc52f6f03590
Jaime Alberto Esquivel Acosta <jaimeesquivel@Jaimes-MacBook-Pro.local>
2023-01-27 18:25:43 -0600

    Models added

---
+++ models/Reaction.js
@@ -0,0 +1,29 @@
+const { Schema, Types } = require('mongoose');
+
+const reactionSchema = new Schema(
+    {
+        reacttionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            max_length: 280,
+        },
+        username: {
+            type: String,
+            required: true,
+        }
+        //CreatedAt -> using timestamps,
+    },
+    {
+        toJSON: {
+          getters: true,
+        },
+        id: false,
+        timestamps: true,
+    }
+);
+
+module.exports = reactionSchema;
---
+++ models/Thought.js
@@ -0,0 +1,31 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            min_length: 1,
+            max_length: 280,
+        },
+        //CreatedAt -> using timestamps,
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [reactionSchema],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false,
+        timestamps: true,
+    }
+);
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
